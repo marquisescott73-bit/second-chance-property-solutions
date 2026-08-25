021dd471fb4ef8adb449e1869658861225c6b77e
marquisescott73-bit <marquisescott73@gmail.com>
2026-08-24 20:23:38 -0700

    Bump Second Chance app cache after installed-resume fix

--- second-chance-pilot/sw.js
+++ second-chance-pilot/sw.js
@@ -1,4 +1,4 @@
-const CACHE='second-chance-complete-v1';
+const CACHE='second-chance-complete-v2';
 const CORE=['./','./index.html','./app.html','./app.css','./steps.js','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
