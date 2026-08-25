6a07d4546eaaa96136f5b94e920325cd2354a388
marquisescott73-bit <marquisescott73@gmail.com>
2026-08-25 13:42:34 -0700

    Cache Second Chance opening cover in app shell

--- second-chance-pilot/sw.js
+++ second-chance-pilot/sw.js
@@ -1,5 +1,5 @@
-const CACHE='second-chance-complete-v3';
-const CORE=['./','./index.html','./app.html','./app.css','./steps.js','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='second-chance-complete-v4';
+const CORE=['./','./index.html','./app.html','./app.css','./steps.js','./app.js','./opening-cover.svg','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET'||new URL(r.url).origin!==self.location.origin)return;if(r.mode==='navigate'){e.respondWith(fetch(r).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(r,copy)).catch(()=>{});return res}).catch(()=>caches.match(r).then(x=>x||caches.match('./index.html'))));return}e.respondWith(caches.match(r).then(hit=>hit||fetch(r).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(r,copy)).catch(()=>{});return res}))) });
